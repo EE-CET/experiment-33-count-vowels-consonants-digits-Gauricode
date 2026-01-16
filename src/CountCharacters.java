0783ca1ecfc988915bc9753bad8879dc0baddf98
unknown <gaurijkjay@gmail.com>
2026-01-16 10:26:15 +0530

    added

--- src/CountCharacters.java
+++ src/CountCharacters.java
@@ -1,10 +1,30 @@
+import java.util.Scanner;
 public class CountCharacters {
+    public static void main(String args[]){
+	Scanner input=new Scanner(System.in);
+	String s=input.nextLine().toLowerCase();
+	int vc=0,cc=0,dc=0,sp=0;
+	for(int i=0;i<s.length();i++){
+		char ch=s.charAt(i);
+		if (ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u' ||
+                ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U') {
+                vc++;
+                }
+            
+            	else if ((ch>='a' && ch<='z') || (ch>='A' && ch<='Z')) {
+                	cc++;
+            	}
+            
+            	else if (ch>='0' && ch<='9') {
+               	 	dc++;
+            	}
+            
+            	else {
+                	sp++;
+            	}
+	}
+	System.out.println(vc+" "+cc+" "+dc+" "+sp);
+        
     
-        // TODO: Read the string line
-        // TODO: Initialize counters for vowels, consonants, digits, special
-        // TODO: Convert to lowercase for easier checking (optional but recommended)
-        // TODO: Iterate through characters and update counters
-        // Hint: Character.isDigit(ch), Character.isLetter(ch) might be useful
-        // TODO: Print the 4 counts separated by spaces
-    
+	}
 }
